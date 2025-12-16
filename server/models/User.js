d1817cf85019a3b6af64c98710c9bb6be0f03e49
Juhi-Karosiya <karosiyajuhi@gmail.com>
2025-12-16 22:18:22 +0530

    user.js chnaged

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,6 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true }
-}, { timestamps: true });
+});
 
 export default mongoose.model('User', UserSchema);
